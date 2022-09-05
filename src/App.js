0c3ea521bbb739a41f4fc0cde5810d39140eefae
anshulv1401 <anshulv1401@gmail.com>
2022-09-05 16:44:30 +0530

    Authorize page added

--- src/App.js
+++ src/App.js
@@ -5,18 +5,20 @@ import {
   Route,
 } from "react-router-dom";
 import LoginPage from './pages/Login';
+import Authorize from './pages/Authorize';
 
 function App() {
   return (
     <div className="min-h-full h-screen flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
-    <div className="max-w-md w-full space-y-8">
-     <BrowserRouter>
-        <Routes>
-            <Route path="/" element={<LoginPage/>} />
-        </Routes>
-      </BrowserRouter>
+      <div className="max-w-md w-full space-y-8">
+        <BrowserRouter>
+          <Routes>
+            <Route path="/login" element={<LoginPage />} />
+            <Route path="/authorize" element={<Authorize />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </div>
-  </div>
   );
 }
 
---
+++ src/common/Errors.js
@@ -0,0 +1,19 @@
+const Error404 = () => {
+    return (
+        <div class="container mx-auto">
+            <h1>404 NOT FOUND</h1>
+        </div>
+    );
+};
+
+
+const Error500 = () => {
+    return (
+        <div class="container mx-auto">
+            <h1>500 INTERNAL SERVER ERROR</h1>
+        </div>
+    );
+};
+
+
+export { Error404, Error500 };
