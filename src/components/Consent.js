586c431216d2964109375cc3f7e08e924d745d7e
anshulv1401 <anshulv1401@gmail.com>
2022-09-13 09:53:59 +0530

    consent page completed

--- src/components/Consent.js
+++ src/components/Consent.js
@@ -55,13 +55,11 @@ export default function Consent() {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log("submit");
         submitConsent();
     }
 
     const handleCancel = (e) => {
         e.preventDefault();
-        console.log("cancel");
     }
 
     let oAuthDetails = JSON.parse(window.localStorage.getItem("oauth_details"));
@@ -69,6 +67,8 @@ export default function Consent() {
     var authorizeScopes = oAuthDetails?.authorizeScopes;
     var essentialClaims = oAuthDetails?.essentialClaims;
     var voluntaryClaims = oAuthDetails?.voluntaryClaims;
+    var clientName = oAuthDetails?.clientName;
+    var logoUrl = oAuthDetails?.logoUrl;
 
     //Handle Login API Integration here
     const submitConsent = async () => {
@@ -113,47 +113,66 @@ export default function Consent() {
         <div className="relative flex flex-col min-w-0 break-words bg-white w-full mb-6 shadow-lg rounded" style={{ background: '#F2F4F4' }}>
             <div className="px-4 py-4 flex-auto" >
                 <form className="mt-8 space-y-6" onSubmit={handleSubmit}>
-                    <div className="-space-y-px">
-                        Client is requesting access to
+                    <div className="flex justify-center">
+                        <img src={logoUrl}></img>
+                    </div>
+
+                    <div className="flex justify-center">
+                        <b>{clientName} is requesting access to : </b>
                     </div>
 
                     {
                         authorizeScopes?.length > 0 &&
                         <>
                             <h2>Authorize Scopes</h2>
-                            {
-                                authorizeScopes?.map(
-                                    (item) => (
-                                        <div className="flex items-center justify-between" key={item}>
-                                            <label labelfor={item} className="inline-flex relative items-center mb-5 cursor-pointer">
-                                                <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleScopeChange} />
-                                                <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                                <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
-                                            </label>
-                                        </div>
+                            <div className='divide-y'>
+                                {
+                                    authorizeScopes?.map(
+                                        (item) => (
+                                            <div key={item}>
+                                                <div class="grid grid-cols-2 gap-4">
+                                                    <div className="flex justify-start">
+                                                        <label labelfor={item} className="inline-flex relative items-center mb-1 mt-1 cursor-pointer">
+                                                            <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleClaimChange} />
+                                                            <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                        </label>
+                                                    </div>
+                                                    <div className="flex justify-end relative items-center mb-1 mt-1 cursor-pointer">
+                                                        <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
                                     )
-                                )
-                            }
+                                }
+                            </div>
                         </>
                     }
-
                     {
                         essentialClaims?.length > 0 &&
                         <>
                             <h2>Essential Claims</h2>
-                            {
-                                essentialClaims?.map(
-                                    (item) => (
-                                        <div className="flex items-center justify-between" key={item}>
-                                            <label labelfor={item} className="inline-flex relative items-center mb-5 cursor-pointer">
-                                                <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleClaimChange} />
-                                                <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                                <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
-                                            </label>
-                                        </div>
+                            <div className='divide-y'>
+                                {
+                                    essentialClaims?.map(
+                                        (item) => (
+                                            <div key={item}>
+                                                <div class="grid grid-cols-2 gap-4">
+                                                    <div className="flex justify-start">
+                                                        <label labelfor={item} className="inline-flex relative items-center mb-1 mt-1 cursor-pointer">
+                                                            <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleClaimChange} />
+                                                            <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                        </label>
+                                                    </div>
+                                                    <div className="flex justify-end relative items-center mb-1 mt-1 cursor-pointer">
+                                                        <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
                                     )
-                                )
-                            }
+                                }
+                            </div>
                         </>
                     }
 
@@ -161,22 +180,29 @@ export default function Consent() {
                         voluntaryClaims?.length > 0 &&
                         <>
                             <h2>Voluntary Claims</h2>
-                            {
-                                voluntaryClaims?.map(
-                                    (item) => (
-                                        <div className="flex items-center justify-between" key={item}>
-                                            <label labelfor={item} className="inline-flex relative items-center mb-5 cursor-pointer">
-                                                <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleClaimChange} />
-                                                <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
-                                                <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
-                                            </label>
-                                        </div>
+                            <div className='divide-y'>
+                                {
+                                    voluntaryClaims?.map(
+                                        (item) => (
+                                            <div key={item}>
+                                                <div class="grid grid-cols-2 gap-4">
+                                                    <div className="flex justify-start">
+                                                        <label labelfor={item} className="inline-flex relative items-center mb-1 mt-1 cursor-pointer">
+                                                            <input type="checkbox" value="" id={item} className="sr-only peer" onChange={handleClaimChange} />
+                                                            <div className="w-9 h-5 bg-gray-200 peer-focus:outline-none peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 rounded-full peer dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-4 after:w-4 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
+                                                        </label>
+                                                    </div>
+                                                    <div className="flex justify-end relative items-center mb-1 mt-1 cursor-pointer">
+                                                        <span className="ml-3 text-sm font-medium text-black-900 dark:text-black-300">{item}</span>
+                                                    </div>
+                                                </div>
+                                            </div>
+                                        )
                                     )
-                                )
-                            }
+                                }
+                            </div>
                         </>
                     }
-
                     {
                         <div>
                             {
@@ -192,25 +218,18 @@ export default function Consent() {
                         )
                     }
 
+                    <div class="grid grid-cols-2 gap-4">
 
-                    <div>
-                        <button className="flex justify-center w-full bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 border border-blue-700 rounded"
-                            onClick={handleSubmit}
-                        >
-                            Allow
-                        </button>
-                    </div>
+                        <button type="button" class="flex justify-center w-full text-gray-900 bg-white border border-gray-300 focus:outline-none hover:bg-gray-100 focus:ring-4 focus:ring-gray-200 font-medium rounded-lg text-sm px-5 py-2.5 mr-2 mb-2 light:bg-gray-800 light:text-white light:border-gray-600 light:hover:bg-gray-700 light:hover:border-gray-600 light:focus:ring-gray-700" onClick={handleCancel}>Cancel</button>
 
-                    <div>
-                        <button className="flex justify-center w-full bg-transparent hover:bg-blue-700 text-blue-700 font-bold hover:text-white py-2 px-4 border border-blue-700 hover:border-transparent rounded"
-                            onClick={handleCancel}
-                        >
-                            Cancel
-                        </button>
+                        <div className="flex justify-end">
+                            <button type="button" class="flex justify-center w-full text-white bg-gradient-to-r from-cyan-500 to-blue-500 hover:bg-gradient-to-bl focus:ring-4 focus:outline-none focus:ring-cyan-300 dark:focus:ring-cyan-800 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-2 mb-2" onClick={handleSubmit}>Allow
+                            </button>
+                        </div>
                     </div>
                 </form>
 
             </div>
-        </div>
+        </div >
     )
 }
--- src/pages/Consent.js
+++ src/pages/Consent.js
@@ -1,13 +1,9 @@
 import React from "react";
-import Header from "../components/Header"
 import Consent from "../components/Consent";
 
 export default function ConsentPage() {
   return (
     <>
-      <Header
-        heading="Login With Mosip"
-      />
       <Consent />
     </>
   )
