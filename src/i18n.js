a4c951ac943841938b87b62f5c7559e6493a3ee7
Anshul <Anshulv1401@gmail.com>
2022-11-08 12:26:51 +0530

    i18n resources object updated

--- src/i18n.js
+++ src/i18n.js
@@ -1,42 +1,16 @@
 import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import LanguageDetector from "i18next-browser-languagedetector";
-
 // Importing translation files
-
 import translationEN from "./locales/en.json";
 import translationHI from "./locales/hi.json";
 import translationAr from "./locales/ar.json";
 
 //Creating object with the variables of imported translation files
 const resources = {
-  en: {
-    consent: translationEN.consent,
-    l1Biometrics: translationEN.l1Biometrics,
-    pin: translationEN.pin,
-    signInOption: translationEN.signInOption,
-    header: translationEN.header,
-    authorize: translationEN.authorize,
-    tabs: translationEN.tabs,
-    errors: translationEN.errors,
-    idpDetails: translationEN.idpDetails,
-    otp: translationEN.otp,
-  },
-  hi: {
-    consent: translationHI.consent,
-    l1Biometrics: translationHI.l1Biometrics,
-    pin: translationHI.pin,
-    signInOption: translationHI.signInOption,
-    header: translationHI.header,
-    authorize: translationHI.authorize,
-    tabs: translationHI.tabs,
-    errors: translationHI.errors,
-    idpDetails: translationHI.idpDetails,
-    otp: translationHI.otp,
-  },
-  ar: {
-    translation: translationAr,
-  },
+  en: translationEN,
+  hi: translationHI,
+  ar: translationAr,
 };
 
 i18n
